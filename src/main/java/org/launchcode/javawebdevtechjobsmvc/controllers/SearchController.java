0b0f6c5cc71b86078345ff39e63add46299e9c4a
Amanda Vanderheyden <aev4882@gmail.com>
2019-12-26 20:59:10 -0600

    almost done!

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,4 +1,5 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
+//package org.launchcode.javawebdevtechjobsmvc.resources.templates.search;
 
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
@@ -6,9 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
+import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.tableChoices;
 
 /**
  * Created by LaunchCode
@@ -17,18 +22,29 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "/search/results")
+    @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+//        model.addAttribute("tables", tableChoices);
         return "search";
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @RequestMapping("search/results")
-    public String displaySearchResults(Model model) {
-        model.addAttribute("searchType", "searchTerm");
-        return "search/results";
+
+    @RequestMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
+        if (searchType.toLowerCase().equals("all")) {
+            jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + tableChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+
+
+        return "search";
     }
-
-
 }
